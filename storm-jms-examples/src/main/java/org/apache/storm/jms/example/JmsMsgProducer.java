f270043c5d05f7f2e87c935b142c262076168a65
Ravindra De Silva <ravindra@code.local>
2017-07-23 21:19:59 -0400

    working jms storm example with gradle

---
+++ storm-jms-examples/src/main/java/org/apache/storm/jms/example/JmsMsgProducer.java
@@ -0,0 +1,28 @@
+package org.apache.storm.jms.example;
+
+import javax.jms.Queue;
+
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JmsMsgProducer {
+	  public static void main(String[] args) {
+		    // init spring context
+		    ApplicationContext ctx = new ClassPathXmlApplicationContext("jms-activemq.xml");
+		         
+		    // get bean from context
+		    JmsMessageSender jmsMessageSender = (JmsMessageSender)ctx.getBean("jmsMessageSender");
+		         
+		    // send to default destination 
+		    jmsMessageSender.send("hello JMS");
+		         
+		    // send to a code specified destination
+		    Queue queue = new ActiveMQQueue("AnotherDest");
+		    jmsMessageSender.send(queue, "hello Another Message");
+		   
+		    // close spring application context
+		    ((ClassPathXmlApplicationContext)ctx).close();
+	}
+	  
+}
